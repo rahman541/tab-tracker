6c9df46363944e304846b15a90baaf935575bf1a
Rahman <rahmanromli@yahoo.com>
2017-09-14 02:56:02 +0800

    Add form

--- client/src/components/Register.vue
+++ client/src/components/Register.vue
@@ -2,7 +2,10 @@
   <div>
     <h1>Register</h1>
 
-    <input type="email" name="email">
+    <input type="email" name="email" placeholder="Email" v-model="email">
+    <input type="password" name="password" placeholder="Password" v-model="password">
+    <button>Register</button>
+
   </div>
 </template>
 
@@ -10,7 +13,19 @@
 export default {
   data () {
     return {
+      email: 'ahs@yahoo.com',
+      password: '1213232'
     }
+  },
+  watch: {
+    email (value) {
+      console.log('email updated', value)
+    }
+  },
+  mounted () {
+    setTimeout(() => {
+      this.email = 'hello world'
+    }, 1000)
   }
 }
 </script>
