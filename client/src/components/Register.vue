e81da44ad42fd544a0bababcaafa81ef704aa96b
Rahman <rahmanromli@yahoo.com>
2017-09-14 02:47:16 +0800

    Add register component

---
+++ client/src/components/Register.vue
@@ -0,0 +1,20 @@
+<template>
+  <div>
+    <h1>Register</h1>
+
+    <input type="email" name="email">
+  </div>
+</template>
+
+<script>
+export default {
+  data () {
+    return {
+    }
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
