0ef96fe0926a3745ba22ef678bbdbcf826a5e427
Rahman <rahmanromli@yahoo.com>
2017-09-14 12:50:16 +0800

    Add page header component

--- client/src/App.vue
+++ client/src/App.vue
@@ -1,12 +1,23 @@
 <template>
   <div id="app">
-    <router-view></router-view>
+    <v-app>
+      <main>
+        <page-header />
+        <v-container>
+          <router-view></router-view>
+        </v-container>
+      </main>
+    </v-app>
   </div>
 </template>
 
 <script>
+import PageHeader from '@/components/Header.vue'
 export default {
-  name: 'app'
+  name: 'app',
+  components: {
+    PageHeader
+  }
 }
 </script>
 
---
+++ client/src/components/Header.vue
@@ -0,0 +1,18 @@
+<template>
+  <v-toolbar fixed class="cyan">
+    <v-toolbar-title class="mr-4">TabTracker</v-toolbar-title>
+  </v-toolbar>
+</template>
+
+<script>
+export default {
+  name: 'hello',
+  data () {
+    return {
+    }
+  }
+}
+</script>
+
+<style scoped>
+</style>
