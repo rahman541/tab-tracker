fe357b3488ff29c7ce53cbc6dfe447baf323e792
Rahman <rahmanromli@yahoo.com>
2017-09-14 13:06:40 +0800

    Add register button link

--- client/src/components/Header.vue
+++ client/src/components/Header.vue
@@ -1,6 +1,21 @@
 <template>
   <v-toolbar fixed class="cyan" dark>
     <v-toolbar-title class="mr-4">TabTracker</v-toolbar-title>
+
+    <!-- TODO: Implement this -->
+    <!-- <v-toolbar-items>
+      <v-btn flat dark>
+        Browse
+      </v-btn>
+    </v-toolbar-items> -->
+
+    <v-spacer></v-spacer>
+
+    <v-toolbar-items>
+      <v-btn flat dark to="register">
+        Sign Up
+      </v-btn>
+    </v-toolbar-items>
   </v-toolbar>
 </template>
 
