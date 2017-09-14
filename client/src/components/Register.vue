980a583e3f659f9fc637a414ef20cab0c996803d
Rahman <rahmanromli@yahoo.com>
2017-09-14 11:48:26 +0800

    Show error in front-end

--- client/src/components/Register.vue
+++ client/src/components/Register.vue
@@ -4,6 +4,9 @@
 
     <input type="email" name="email" placeholder="Email" v-model="email">
     <input type="password" name="password" placeholder="Password" v-model="password">
+
+    <div v-html="error" class="error" /><br/>
+
     <button @click="register">Register</button>
 
   </div>
@@ -14,22 +17,26 @@ import AuthenticationService from '@/services/AuthenticationService'
 export default {
   data () {
     return {
-      email: 'ahs@yahoo.com',
-      password: '1213232'
+      email: '',
+      password: '',
+      error: null
     }
   },
   methods: {
     async register () {
-      const response = await AuthenticationService.register({
-        email: this.email,
-        password: this.password
-      })
-      console.log(response.data)
+      try {
+        const response = await AuthenticationService.register({
+          email: this.email,
+          password: this.password
+        })
+      } catch (error) {
+        this.error = error.response.data.error
+      }
     }
   }
 }
 </script>
 
 <style scoped>
-
+.error { color: red; }
 </style>
