39a2bdacb49c748376257e543912bec029ba7231
Rahman <rahmanromli@yahoo.com>
2017-09-14 11:20:37 +0800

    Add joi library for validation

--- server/src/controller/AuthenticationController.js
+++ server/src/controller/AuthenticationController.js
@@ -6,9 +6,8 @@ module.exports = {
       const user = await User.create(req.body)
       res.send(user.toJSON())
     } catch (err) {
-        console.log(err)
         res.status(400).send({
-        error: 'This email account is already used.'
+        error: 'This email account is already in used.'
       })
     }
   }
