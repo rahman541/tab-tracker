050e113b7594e2d4ced1ffeaf45d22d1ef75d6ba
Rahman <rahmanromli@yahoo.com>
2017-09-15 12:45:39 +0800

    Add search panel & separate song panel to another component

--- client/src/components/Songs/Index.vue
+++ client/src/components/Songs/Index.vue
@@ -1,33 +1,8 @@
 <template>
   <v-layout column>
     <v-flex xs6 offset-xs3>
-      <panel title="Songs">
-        <v-btn slot="action" fab class="cyan accent-2" light medium absolute right middle :to="{name: 'songs-create'}">
-          <v-icon>add</v-icon>
-        </v-btn>
-        <div class="song" v-for="song in songs" :key="song.id">
-          <v-layout>
-            <v-flex xs6>
-              <div class="song-title">
-                {{ song.title }}
-              </div>
-              <div class="song-artist">
-                {{ song.artist }}
-              </div>
-              <div class="song-genre">
-                {{ song.genre }}
-              </div>
-
-              <v-btn dark class="cyan" :to="{name:'song', params: {songId: song.id}}">
-                View
-              </v-btn>
-            </v-flex>
-            <v-flex xs6>
-              <img class="album-image" :src="song.albumImageUrl" />
-            </v-flex>
-          </v-layout>
-        </div>
-      </panel>
+      <songs-search-panel />
+      <songs-panel />
     </v-flex>
   </v-layout>
 </template>
@@ -35,9 +10,13 @@
 <script>
 import SongsService from '@/services/SongsService'
 import Panel from '@/components/Panel'
+import SongsPanel from './SongsPanel'
+import SongsSearchPanel from './SongsSearchPanel'
 export default {
   components: {
-    Panel
+    Panel,
+    SongsPanel,
+    SongsSearchPanel
   },
   data () {
     return {
@@ -51,26 +30,4 @@ export default {
 </script>
 
 <style scoped>
-.song {
-  padding: 20px;
-  height: 330px;
-  overflow: hidden;
-}
-
-.song-title {
-  font-size: 30px;
-}
-
-.song-artist {
-  font-size: 24px;
-}
-
-.song-genre {
-  font-size: 18px;
-}
-
-.album-image {
-  width: 70%;
-  margin: 0 auto;
-}
 </style>
---
+++ client/src/components/Songs/SongsPanel.vue
@@ -0,0 +1,72 @@
+<template>
+  <panel title="Songs">
+    <v-btn slot="action" fab class="cyan accent-2" light medium absolute right middle :to="{name: 'songs-create'}">
+      <v-icon>add</v-icon>
+    </v-btn>
+    <div class="song" v-for="song in songs" :key="song.id">
+      <v-layout>
+        <v-flex xs6>
+          <div class="song-title">
+            {{ song.title }}
+          </div>
+          <div class="song-artist">
+            {{ song.artist }}
+          </div>
+          <div class="song-genre">
+            {{ song.genre }}
+          </div>
+
+          <v-btn dark class="cyan" :to="{name:'song', params: {songId: song.id}}">
+            View
+          </v-btn>
+        </v-flex>
+        <v-flex xs6>
+          <img class="album-image" :src="song.albumImageUrl" />
+        </v-flex>
+      </v-layout>
+    </div>
+  </panel>
+</template>
+
+<script>
+import SongsService from '@/services/SongsService'
+import Panel from '@/components/Panel'
+export default {
+  components: {
+    Panel
+  },
+  data () {
+    return {
+      songs: null
+    }
+  },
+  async mounted () {
+    this.songs = (await SongsService.index()).data
+  }
+}
+</script>
+
+<style scoped>
+.song {
+  padding: 20px;
+  height: 330px;
+  overflow: hidden;
+}
+
+.song-title {
+  font-size: 30px;
+}
+
+.song-artist {
+  font-size: 24px;
+}
+
+.song-genre {
+  font-size: 18px;
+}
+
+.album-image {
+  width: 70%;
+  margin: 0 auto;
+}
+</style>
