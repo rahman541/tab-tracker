73941bac8ac58fcee47086780089086b213004e4
Rahman <rahmanromli@yahoo.com>
2017-09-15 22:26:12 +0800

    Add recently viewed song in front end

--- client/src/components/Songs/Index.vue
+++ client/src/components/Songs/Index.vue
@@ -2,6 +2,7 @@
   <v-layout>
     <v-flex xs6>
       <songs-bookmarks />
+      <recently-viewed-songs class="mt-2" />
     </v-flex>
     <v-flex xs6 class="ml-2">
       <songs-search-panel />
@@ -15,11 +16,13 @@ import SongsService from '@/services/SongsService'
 import SongsPanel from './SongsPanel'
 import SongsSearchPanel from './SongsSearchPanel'
 import SongsBookmarks from './SongsBookmarks'
+import RecentlyViewedSongs from './RecentlyViewedSongs'
 export default {
   components: {
     SongsPanel,
     SongsSearchPanel,
-    SongsBookmarks
+    SongsBookmarks,
+    RecentlyViewedSongs
   },
   data () {
     return {
