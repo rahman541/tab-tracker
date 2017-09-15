db2bdbece74f2b06249e8c986b0956bf2a687900
Rahman <rahmanromli@yahoo.com>
2017-09-15 10:14:14 +0800

    Update view song layout

--- client/src/components/ViewSong.vue
+++ client/src/components/ViewSong.vue
@@ -1,28 +1,47 @@
 <template>
-  <v-layout>
-    <v-flex xs6>
-      <panel title="Song Metadata">
-        <v-layout>
-          <v-flex xs6>
-            <div class="song-title">
-              {{ song.title }}
-            </div>
-            <div class="song-artist">
-              {{ song.artist }}
-            </div>
-            <div class="song-genre">
-              {{ song.genre }}
-            </div>
-          </v-flex>
+  <div>
+    <v-layout>
+      <v-flex xs6>
+        <panel title="Song Metadata">
+          <v-layout>
+            <v-flex xs6>
+              <div class="song-title">
+                {{ song.title }}
+              </div>
+              <div class="song-artist">
+                {{ song.artist }}
+              </div>
+              <div class="song-genre">
+                {{ song.genre }}
+              </div>
+            </v-flex>
 
-          <v-flex xs6>
-            <img class="album-image" :src="song.albumImageUrl" /> <br/>
-            {{ song.album }}
-          </v-flex>
-        </v-layout>
-      </panel>
-    </v-flex>
-  </v-layout>
+            <v-flex xs6>
+              <img class="album-image" :src="song.albumImageUrl" /> <br/>
+              {{ song.album }}
+            </v-flex>
+          </v-layout>
+        </panel>
+      </v-flex>
+    </v-layout>
+
+    <v-layout>
+      <v-flex xs6>
+        <panel title="Song Metadata">
+          Youtube|
+        </panel>
+      </v-flex>
+
+      <v-flex xs6 class="ml-2">
+        <panel title="Lyrics">
+          <textarea
+            readonly
+            v-model="song.lyrics"
+          ></textarea>
+        </panel>
+      </v-flex>
+    </v-layout>
+  </div>
 </template>
 <script>
 import SongsService from '@/services/SongsService'
@@ -66,5 +85,16 @@ export default {
 .album-image {
   width: 70%;
   margin: 0 auto;
+}
+
+textarea {
+  width: 100%;
+  font-family: monospace;
+  border: none;
+  height: 600px;
+  border-style: none;
+  border-color: transparent;
+  overflow: auto;
+  padding: 20px;
 }
 </style>
