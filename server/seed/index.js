0b22fa456b965cc205d12265267b1864e95dcf22
Rahman <rahmanromli@yahoo.com>
2017-09-15 15:19:37 +0800

    Add association table for Song + User = Bookmark

--- server/seed/index.js
+++ server/seed/index.js
@@ -1,12 +1,14 @@
 const {
   sequelize,
   Song,
-  User
+  User,
+  Bookmark
 } = require('../src/models')
 
 const Promise = require('bluebird')
 const songs = require('./songs.json')
 const users = require('./user.json')
+const bookmarks = require('./bookmark.json')
 
 sequelize.sync({force: true}).then(async function () {
 
@@ -19,6 +21,12 @@ sequelize.sync({force: true}).then(async function () {
   await Promise.all(
     songs.map(song => {
       Song.create(song)
+    })
+  )
+  
+  await Promise.all(
+    bookmarks.map(bookmark => {
+      Bookmark.create(bookmark)
     })
   )
 
---
+++ server/src/models/Bookmark.js
@@ -0,0 +1,12 @@
+// Sausage: Use for association
+
+module.exports = (sequelize, DataTypes) => {
+  const Bookmark = sequelize.define('Bookmark', {})
+
+  Bookmark.associate = function (models) {
+    Bookmark.belongsTo(models.User)
+    Bookmark.belongsTo(models.Song)
+  }
+
+  return Bookmark
+}
