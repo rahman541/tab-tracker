669e9dbc213986e269098c79805b85964d91879b
Rahman <rahmanromli@yahoo.com>
2017-09-15 15:03:41 +0800

    Add seed data on backend. Run: node seed

---
+++ server/seed/index.js
@@ -0,0 +1,25 @@
+const {
+  sequelize,
+  Song,
+  User
+} = require('../src/models')
+
+const Promise = require('bluebird')
+const songs = require('./songs.json')
+const users = require('./user.json')
+
+sequelize.sync({force: true}).then(async function () {
+
+  await Promise.all(
+    users.map(user => {
+      User.create(user)
+    })
+  )
+  
+  await Promise.all(
+    songs.map(song => {
+      Song.create(song)
+    })
+  )
+
+})
