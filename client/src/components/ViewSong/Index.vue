77711d3c1409e82cb9f5c60aaff622f56c5c3f1b
Rahman <rahmanromli@yahoo.com>
2017-09-15 10:30:53 +0800

    Move content of sub vue

--- client/src/components/ViewSong/Index.vue
+++ client/src/components/ViewSong/Index.vue
@@ -26,6 +26,7 @@
 <script>
 import SongsService from '@/services/SongsService'
 import Panel from '@/components/Panel'
+import SongMetadata from './SongMetadata'
 export default {
   data () {
     return {
@@ -38,35 +39,13 @@ export default {
     console.log(this.song)
   },
   components: {
-    Panel
+    Panel,
+    SongMetadata
   }
 }
 </script>
 
 <style scoped>
-.song {
-  padding: 20px;
-  height: 330px;
-  overflow: hidden;
-}
-
-.song-title {
-  font-size: 30px;
-}
-
-.song-artist {
-  font-size: 24px;
-}
-
-.song-genre {
-  font-size: 18px;
-}
-
-.album-image {
-  width: 70%;
-  margin: 0 auto;
-}
-
 textarea {
   width: 100%;
   font-family: monospace;
--- client/src/components/ViewSong/SongMetadata.vue
+++ client/src/components/ViewSong/SongMetadata.vue
@@ -22,12 +22,38 @@
 </template>
 
 <script>
+import Panel from '@/components/Panel'
 export default {
-  props:[
+  props: [
     'song'
-  ]
+  ],
+  components: {
+    Panel
+  }
 }
 </script>
 
 <style scoped>
+.song {
+  padding: 20px;
+  height: 330px;
+  overflow: hidden;
+}
+
+.song-title {
+  font-size: 30px;
+}
+
+.song-artist {
+  font-size: 24px;
+}
+
+.song-genre {
+  font-size: 18px;
+}
+
+.album-image {
+  width: 70%;
+  margin: 0 auto;
+}
 </style>
