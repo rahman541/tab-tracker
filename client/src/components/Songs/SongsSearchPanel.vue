543e87b0d4f9873a6d9177851e85e3d9d4b16ed6
Rahman <rahmanromli@yahoo.com>
2017-09-15 13:02:31 +0800

    Update search panel: watch search box typing & change url accordingly

--- client/src/components/Songs/SongsSearchPanel.vue
+++ client/src/components/Songs/SongsSearchPanel.vue
@@ -1,11 +1,38 @@
 <template>
   <panel title="Search">
-    
+    <v-text-field
+      v-model="search"
+      label="Search by song title, artist, album or genre"
+    ></v-text-field>
   </panel>
 </template>
 
 <script>
 export default {
+  data () {
+    return {
+      search: ''
+    }
+  },
+  watch: {
+    search (value) {
+      const route = {
+        name: 'songs'
+      }
+      if (this.search !== '') {
+        route.query = {
+          search: this.search
+        }
+      }
+      this.$router.push(route)
+    },
+    '$route.query.search': {
+      immediate: true,
+      handler (value) {
+        this.search = value
+      }
+    }
+  }
 }
 </script>
 
