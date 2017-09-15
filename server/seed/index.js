62b52e21ee4616923fe1ba5d5beb8a3e667aad5e
Rahman <rahmanromli@yahoo.com>
2017-09-15 23:02:41 +0800

    Add history model & seed data

--- server/seed/index.js
+++ server/seed/index.js
@@ -2,13 +2,15 @@ const {
   sequelize,
   Song,
   User,
-  Bookmark
+  Bookmark,
+  History
 } = require('../src/models')
 
 const Promise = require('bluebird')
 const songs = require('./songs.json')
 const users = require('./user.json')
 const bookmarks = require('./bookmark.json')
+const histories = require('./history.json')
 
 sequelize.sync({force: true}).then(async function () {
 
@@ -27,6 +29,12 @@ sequelize.sync({force: true}).then(async function () {
   await Promise.all(
     bookmarks.map(bookmark => {
       Bookmark.create(bookmark)
+    })
+  )
+  
+  await Promise.all(
+    histories.map(history => {
+      History.create(history)
     })
   )
 
